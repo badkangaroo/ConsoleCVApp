9c9644d7c94317d33b61d064b9d14b3c15159504
Alex Okita <alex.okita@gmail.com>
2016-10-07 01:01:32 -0700

    added imread

--- ConsoleCVApp/ConsoleCVApp.cpp
+++ ConsoleCVApp/ConsoleCVApp.cpp
@@ -2,10 +2,21 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include "opencv2/highgui.hpp"
 
-
+using namespace cv;
+using namespace std;
 int main()
 {
-    return 0;
+	Mat img = imread("image.jpg", CV_LOAD_IMAGE_ANYCOLOR);
+	namedWindow("window", WINDOW_OPENGL);
+	cout << img.cols;
+	// the Mat data type
+	Mat gimg = img.clone();
+	
+	imshow("window", gimg);
+	waitKey(0);
+	return 0;
 }
 
