e107d70d2c8a36428c29a962527993f4de94d5a4
Alex Okita <alex.okita@gmail.com>
2016-10-08 21:52:06 -0700

    hey cool, got some stuff doing what i think it's going to do!

--- ConsoleCVApp/ConsoleCVApp.cpp
+++ ConsoleCVApp/ConsoleCVApp.cpp
@@ -3,20 +3,38 @@
 
 #include "stdafx.h"
 #include <iostream>
-#include "opencv2/highgui.hpp"
+#include <opencv2\core.hpp>
+#include <opencv2\highgui.hpp>
+#include <opencv2\imgproc.hpp>
 
-using namespace cv;
 using namespace std;
+using namespace cv;
+
+void something();
+
 int main()
 {
 	Mat img = imread("image.jpg", CV_LOAD_IMAGE_ANYCOLOR);
-	namedWindow("window", WINDOW_OPENGL);
+	namedWindow("processed", cv::WINDOW_OPENGL);
+	namedWindow("un processed", cv::WINDOW_OPENGL);
 	cout << img.cols;
 	// the Mat data type
 	Mat gimg = img.clone();
-	
-	imshow("window", gimg);
+	Size s;
+	s.height = 7;
+	s.width = 7;
+	GaussianBlur(img, gimg, s, 4.0, 4.0, 4);
+
+	// doing something
+	something();
+	imshow("processed", gimg);
+	imshow("un processed", img);
 	waitKey(0);
 	return 0;
 }
 
+void something()
+{
+	// doing something here
+	cout << "this is something" << endl;
+}
